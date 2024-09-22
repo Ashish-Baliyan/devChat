21ba85d31650cedd77789b7da174d95f444a158a
=Ashish-Baliyan <=ashish.baliyan78@gmail.com>
2024-09-22 12:31:40 +0530

    push app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express =require('express');
 
 const app = express();
 
-app.use((req,res) => {
-    res.send("hello from the server!");
-});
+app.use("/user", (req,res, next)=>{
+    res.send("route handler 1");
+    next();
+},
+(req,res)=>{
+    res.send("route handler 2");
+}
+);
 
 app.listen(3001, ()=> {
 console.log("server is successfully listening on port 3001");
